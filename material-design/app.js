9bd34cd16e5b79c5e491b4feaac1f12e39ca59a7
Gustavo de Paula <gdepaula@gmail.com>
2021-01-11 14:51:10 -0300

    Update the select element

--- material-design/app.js
+++ material-design/app.js
@@ -1,5 +1,11 @@
-const shippingForm = document.querySelector('#crane-shipping-form');
-shippingForm.addEventListener('submit', (evt) => {
+import { MDCRipple } from "@material/ripple";
+import { MDCSelect } from "@material/select";
+
+const shippingForm = document.querySelector("#crane-shipping-form");
+shippingForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  alert('Success!');
+  alert("Success!");
 });
+
+new MDCRipple(document.querySelector(".mdc-button"));
+new MDCSelect(document.querySelector(".mdc-select"));
