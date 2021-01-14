00b72b8cf7548b704a23208a3a220c150986f349
Gustavo de Paula <gdepaula@gmail.com>
2021-01-14 16:32:55 -0300

    MDC-111 Web: Update the text fields

--- material-design/mdc-111/app.js
+++ material-design/mdc-111/app.js
@@ -1,10 +1,18 @@
 import { MDCRipple } from "@material/ripple";
 import { MDCSelect } from "@material/select";
+import { MDCTextField } from "@material/textfield";
 
 const shippingForm = document.querySelector("#crane-shipping-form");
 shippingForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   alert("Success!");
+});
+
+const textFieldElements = [].slice.call(
+  document.querySelectorAll(".mdc-text-field")
+);
+textFieldElements.forEach((textFieldEl) => {
+  new MDCTextField(textFieldEl);
 });
 
 new MDCRipple(document.querySelector(".mdc-button"));
