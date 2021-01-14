fe22b5d525b5fcf97c053f756e24ae103a1b5a33
Gustavo de Paula <gdepaula@gmail.com>
2021-01-14 11:45:53 -0300

    MDC-111 Web: Update the button

--- material-design/mdc-111/app.js
+++ material-design/mdc-111/app.js
@@ -1,5 +1,9 @@
+import { MDCRipple } from "@material/ripple";
+
 const shippingForm = document.querySelector("#crane-shipping-form");
 shippingForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   alert("Success!");
 });
+
+new MDCRipple(document.querySelector(".mdc-button"));
