e750b586cfd8e8bfbaf36c3c0e2778e42cf9551d
Gustavo de Paula <gdepaula@gmail.com>
2021-10-14 14:39:21 -0300

    Making the Mobile Navigation Work

--- html-css-course/09-Omnifood-Optimizations/js/script.js
+++ html-css-course/09-Omnifood-Optimizations/js/script.js
@@ -5,15 +5,24 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-h1.addEventListener("click", function () {
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
-});
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
 
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
